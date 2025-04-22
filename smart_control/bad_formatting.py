2647a18f993c216399df576277fccc44c43c7c30
Michael Rossetti <mjrossetti@google.com>
2025-04-22 14:48:57 +0000

    Test pre-commit hook

---
+++ smart_control/bad_formatting.py
@@ -0,0 +1,6 @@
+# this line is way too long...
+console.log("This line is way too long.......................................................................")
+
+
+if True:
+    # this line is indented using four spaces instead of two
