960b01bd86df3bb82c1ab4c90577cd03491955fd
Michael Rossetti <mjrossetti@google.com>
2025-04-22 18:50:19 +0000

    Pre-commit hook test is successful

---
+++ smart_control/bad_formatting.py
@@ -0,0 +1,10 @@
+# line too long:
+print(
+    "This line is wayyyy toooo longgggg"
+    " ................................................"
+)
+
+
+# uses four spaces instead of two:
+if 2 + 2 == 4:
+  print("YEAH")
